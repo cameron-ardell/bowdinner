24908a5c085e4f1b27447f1c57b92a47ff15b4f2
Sophia M. Ardell <sardell@searles224-09.bowdoin.edu>
2016-05-17 21:01:40 -0400

    added the new files I forgot to add before

---
+++ testingTables/Interact.swift
@@ -0,0 +1,14 @@
+//
+//  Interact.swift
+//  testingTables
+//
+//  Created by Sophia M. Ardell on 5/17/16.
+//  Copyright © 2016 Sophia M. Ardell. All rights reserved.
+//
+
+import UIKit
+
+class Interact: UIPercentDrivenInteractiveTransition {
+    var hasStarted = false
+    var shouldFinish = false
+}
